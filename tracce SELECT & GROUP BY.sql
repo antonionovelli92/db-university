eb92507996c8b37b72eb4ae2827d062e3e77c6ba
antonionovelli92 <anto.novelli@hotmail.it>
2023-02-14 16:34:54 +0100

    3. ex

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -7,7 +7,7 @@ SELECT `date_of_birth` FROM `students` WHERE YEAR(`date_of_birth`) LIKE '1990';
 SELECT `cfu` FROM `courses` WHERE `cfu` > 10;
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
-
+SELECT `date_of_birth` AS 'Più di 30 anni' FROM `students` WHERE `date_of_birth` < '1992-%-%';
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
