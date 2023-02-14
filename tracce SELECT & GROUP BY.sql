5ac66ab54df8c9d02858413a6b32bf44371237b0
antonionovelli92 <anto.novelli@hotmail.it>
2023-02-14 17:08:45 +0100

    fix 1. ex

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -5,6 +5,9 @@ SELECT `date_of_birth`
 FROM `students` 
 WHERE YEAR(`date_of_birth`) LIKE '1990';
 
+--! fix post ticket
+SELECT `date_of_birth` FROM `students` WHERE YEAR(`date_of_birth`) = '1990';
+
 -- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
 SELECT `cfu` 
 FROM `courses` 
